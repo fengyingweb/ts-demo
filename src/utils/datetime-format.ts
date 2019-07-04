98e864fbdc7fa8c8c88f95cdbb24c14d6c3c7814
fengying <huangjun_web@163.com>
2019-07-04 19:31:50 +0800

    代码格式化

--- src/utils/datetime-format.ts
+++ src/utils/datetime-format.ts
@@ -1,14 +1,14 @@
-export function dateTimeFormat (date: any, fmt: string = 'YYYY-MM-DD HH:mm:ss'): string {
+export function dateTimeFormat(date: any, fmt: string = 'YYYY-MM-DD HH:mm:ss'): string {
   if (!date) {
-    return ''
+    return '';
   }
   if (typeof date === 'string') {
-    date = new Date(date.replace(/-/g, '/'))
+    date = new Date(date.replace(/-/g, '/'));
   }
   if (typeof date === 'number') {
-    date = new Date(date)
+    date = new Date(date);
   }
-  var o = {
+  let o = {
     'M+': date.getMonth() + 1,
     'D+': date.getDate(),
     'h+': date.getHours() % 12 === 0 ? 12 : date.getHours() % 12,
@@ -16,27 +16,27 @@ export function dateTimeFormat (date: any, fmt: string = 'YYYY-MM-DD HH:mm:ss'):
     'm+': date.getMinutes(),
     's+': date.getSeconds(),
     'q+': Math.floor((date.getMonth() + 3) / 3),
-    'S': date.getMilliseconds()
-  }
-  var week = {
+    'S': date.getMilliseconds(),
+  };
+  let week = {
     '0': '\u65e5',
     '1': '\u4e00',
     '2': '\u4e8c',
     '3': '\u4e09',
     '4': '\u56db',
     '5': '\u4e94',
-    '6': '\u516d'
-  }
+    '6': '\u516d',
+  };
   if (/(Y+)/.test(fmt)) {
-    fmt = fmt.replace(RegExp.$1, (date.getFullYear() + '').substr(4 - RegExp.$1.length))
+    fmt = fmt.replace(RegExp.$1, (date.getFullYear() + '').substr(4 - RegExp.$1.length));
   }
   if (/(E+)/.test(fmt)) {
-    fmt = fmt.replace(RegExp.$1, ((RegExp.$1.length > 1) ? (RegExp.$1.length > 2 ? '\u661f\u671f' : '\u5468') : '') + week[date.getDay() + ''])
+    fmt = fmt.replace(RegExp.$1, ((RegExp.$1.length > 1) ? (RegExp.$1.length > 2 ? '\u661f\u671f' : '\u5468') : '') + week[date.getDay() + '']);
   }
-  for (var k in o) {
+  for (let k in o) {
     if (new RegExp('(' + k + ')').test(fmt)) {
-      fmt = fmt.replace(RegExp.$1, (RegExp.$1.length === 1) ? (o[k]) : (('00' + o[k]).substr(('' + o[k]).length)))
+      fmt = fmt.replace(RegExp.$1, (RegExp.$1.length === 1) ? (o[k]) : (('00' + o[k]).substr(('' + o[k]).length)));
     }
   }
-  return fmt
+  return fmt;
 }
